83eb65a1942232c1a646915c12550884173b024f
Pedroveiga19 <pedrodaveiga19@gmail.com>
2025-11-12 12:02:06 -0300

    feito login e permissões

--- web_system/relacionamentos/views/reporter.py
+++ web_system/relacionamentos/views/reporter.py
@@ -3,19 +3,24 @@ import string
 from relacionamentos.models.reporter import Reporter
 from relacionamentos.forms.reporter_form import ReporterForm
 from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.decorators import login_required,permission_required
+from django.views.decorators.http import require_http_methods
 import random
 
-
+@login_required(login_url='/login/')
+@permission_required("usuario.can_list_reporter")
 def reporter_list(request):
     reporters = Reporter.objects.all()
     context = {"reporters":reporters}
     return render(request, "reporter/list.html", context) 
 
+@require_http_methods(['GET'])
 def reporter_detail(request,pk):    
     reporter = Reporter.objects.get(id=pk)
     context = {"reporter":reporter}
     return render(request, "reporter/read.html", context)
 
+@require_http_methods(['GET'])
 def reporter_delete(request,pk):    
     reporter = get_object_or_404(Reporter, pk=pk)
     try:
@@ -34,6 +39,7 @@ def reporter_delete(request,pk):
     return render(request, "reporter/delete.html", context)
 
 
+@require_http_methods(['GET'])
 def reporter_gerar_codigo(request,reporter_id):
     reporter = get_object_or_404(Reporter, pk=reporter_id)
     try:
@@ -46,7 +52,7 @@ def reporter_gerar_codigo(request,reporter_id):
         print(e)
         return render(request, "reporter/list.html")
 
-
+@require_http_methods(['GET'])
 def create(request):
     if request.method == "POST":
         form = ReporterForm(request.POST)
@@ -63,7 +69,7 @@ def create(request):
 
     return render(request,'reporter/create.html',context)
 
-
+@require_http_methods(['GET'])
 def update(request,reporter_id):
     reporter = get_object_or_404(Reporter, pk=reporter_id)
     if request.method == "POST":
--- web_system/web_system/urls.py
+++ web_system/web_system/urls.py
@@ -1,11 +1,11 @@
-from tkinter.font import names
-
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from web_system.views.estaticas import index
 from web_system.views.contato import contact
 from .views.contato_classe import ContactView
+from django.contrib.auth import views as auth_views
+from .forms.custom_login_form import CustomLoginForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,5 +13,10 @@ urlpatterns = [
     path('classe/contato/',ContactView.as_view(),name="classe_contato"),
     path('funcao/contato/',contact,name='funcao_contato'),
    # path('funcao/search/',views.buscar,name='funcao_buscar'),
-    path('', index, name="index")
+    path('', index, name="index"),
+    path('accounts/login/',
+         auth_views.LoginView.as_view(template_name="accounts/login.html",
+                                      authentication_form=CustomLoginForm), name="login"),
+    path('accounts/', include('django.contrib.auth.urls')),
+
 ]
